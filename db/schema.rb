9032fe5e75d513b267c8ff361230930bc665a57a
Nate Hill <natanio@gmail.com>
2014-11-19 11:41:02 +0900

    Add username to user, display on chats

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118060634) do
+ActiveRecord::Schema.define(version: 20141119023101) do
 
   create_table "hangouts", force: true do |t|
     t.string   "name"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20141118060634) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
