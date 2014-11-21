efff67b4592b51699b848089aec9580a9bd6b330
Nate Hill <natanio@gmail.com>
2014-11-22 06:45:01 +0900

    Add tag cloud

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,12 @@ module PostsHelper
 	def tag_links(tags)
   		tags.split(",").map{|tag| link_to tag.strip, language_hangout_tag_path(@language, @hangout, tag.strip) }.join(", ") 
 	end
+
+	def tag_cloud(tags, classes)
+  		max = tags.sort_by(&:count).last
+  		tags.each do |tag|
+    		index = tag.count.to_f / max.count * (classes.size-1)
+    		yield(tag, classes[index.round])
+  		end
+	end
 end
