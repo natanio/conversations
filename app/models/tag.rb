d8283a3cf4a3abf565c6af6ed67b29415cad9099
Nate Hill <natanio@gmail.com>
2014-11-22 07:03:51 +0900

    Add tags.name to group for PG

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,6 @@ class Tag < ActiveRecord::Base
 	has_many :posts, through: :taggings
 
 	def self.counts
-  		self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+  		self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("tags.name, taggings.tag_id")
 	end
 end
