8b0575b2921e043d3603a8212c47da593f9c8c14
Nate Hill <natanio@gmail.com>
2014-11-29 14:21:37 +0900

    Get automatic username working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 class User < ActiveRecord::Base
-  after_create :set_username
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :set_username
 
   has_many :hangouts
   has_many :posts
 
   private
     def set_username
-       self.username = "user-#{ SecureRandom.hex(10)}"
+      self.username = "user-#{ SecureRandom.hex(10) }"
     end
 end
