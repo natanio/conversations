a7470ba347211782b15dc594d9b93832000f15cd
Nate Hill <natanio@gmail.com>
2014-12-02 14:38:45 +0900

    change sign in url to profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   protected
+
+  def after_sign_in_path_for(resource)
+    profile_path(resource)
+  end
+
   def configure_permitted_parameters
 	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
