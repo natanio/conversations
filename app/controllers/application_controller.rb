39a0ff30c04fd76bd75cd38c730f23caed0e8136
Nate Hill <natanio@gmail.com>
2014-12-03 11:22:41 +0900

    Add profile link to menu, fix login redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-    profile_path(resource)
+    profile_path(resource.profile)
   end
 
   def configure_permitted_parameters
