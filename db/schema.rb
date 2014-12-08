513667e2789ca9afb5ea0ddc44bd1bc1f9c94b6b
Nate Hill <natanio@gmail.com>
2014-12-09 06:09:38 +0900

    fix hangout id mistake in tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141204025034) do
+ActiveRecord::Schema.define(version: 20141208083010) do
 
   create_table "follows", force: true do |t|
     t.integer  "followable_id",                   null: false
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20141204025034) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "hangout_id"
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name"
