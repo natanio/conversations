63d1faefb1486b28148287e0aac855f5efe7e8ac
Nate Hill <natanio@gmail.com>
2014-12-11 10:11:10 +0900

    Change language index style

--- app/helpers/languages_helper.rb
+++ app/helpers/languages_helper.rb
@@ -10,4 +10,12 @@ module LanguagesHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def scheduled_hangout_count(language)
+  	Hangout.where(["language_id = ? and start_time > ?", language.id, Time.now]).count
+  end
+
+  def live_hangout_count(language)
+  	Hangout.where(["language_id = ? and start_time <= ? and end_time > ?", language.id, Time.now, Time.now]).count
+  end
 end
