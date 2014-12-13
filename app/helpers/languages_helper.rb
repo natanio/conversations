8e2dd2227473f7ed4b023fbb41797a018d7bb571
Nate Hill <natanio@gmail.com>
2014-12-14 06:47:26 +0900

    take private future chats out of chat count

--- app/helpers/languages_helper.rb
+++ app/helpers/languages_helper.rb
@@ -12,7 +12,7 @@ module LanguagesHelper
   end
 
   def scheduled_hangout_count(language)
-  	Hangout.where(["language_id = ? and start_time > ?", language.id, Time.now]).count
+  	Hangout.where(["language_id = ? and private = ? and start_time > ?", language.id, false, Time.now]).count
   end
 
   def live_hangout_count(language)
